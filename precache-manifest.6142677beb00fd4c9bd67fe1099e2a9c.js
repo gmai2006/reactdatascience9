a551ad032b01401b64cf87b8e3dbb2a2f80513ba
Paul Mai <gmai2006@gmail.com>
2021-01-29 20:56:53 -0800

    Updates

---
+++ precache-manifest.6142677beb00fd4c9bd67fe1099e2a9c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b7f84430d59b6a0c7f5",
+    "url": "/static/js/main.4b7f8443.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ea4343e97df68282631d",
+    "url": "/static/js/2.ea4343e9.chunk.js"
+  },
+  {
+    "revision": "a3f0075dcd6a906a013e499b9a7747fb",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/reactdatascience9/precache-manifest.13a08f887800832f87b30c985e3f67be.js"
+  "/precache-manifest.6142677beb00fd4c9bd67fe1099e2a9c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/reactdatascience9/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
