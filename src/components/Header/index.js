e43332bbc0398fbc011cf27ab42d1b1923871492
Paul Mai <gmai2006@gmail.com>
2021-11-13 21:32:16 -0800

    update header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
+import Container from 'react-bootstrap/Container';
+
 import "./style.css";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -8,29 +10,31 @@ import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
 const Header = () => {
   return (
     <React.Fragment>
-      <Navbar expand="lg" className="nav ml-auto">
-        <Nav className="header-link" id="leftmargin">
-          <Nav.Item>
-            <Nav.Link
-              href="tel:425-770-6899"
-              className="header-text"
-              id="leftmargin"
-            >
-              <FontAwesomeIcon icon={faPhone} /> Any questions? Call us: (425)
-              770-6899
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link
-              href="mailto:paul.mai@datascience9.com"
-              className="header-text"
-            >
-              <FontAwesomeIcon icon={faEnvelope} /> Mail us:
-              paul.mai@datascience9.com
-            </Nav.Link>
-          </Nav.Item>
-        </Nav>
-      </Navbar>
+      <Container>
+        <Navbar expand="lg" className="nav ml-auto">
+          <Nav className="header-link">
+            <Nav.Item>
+              <Nav.Link
+                href="tel:425-770-6899"
+                className="header-text"
+                id="leftmargin"
+              >
+                <FontAwesomeIcon icon={faPhone} /> Any questions? Call us: (425)
+                770-6899
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                href="mailto:paul.mai@datascience9.com"
+                className="header-text"
+              >
+                <FontAwesomeIcon icon={faEnvelope} /> Mail us:
+                paul.mai@datascience9.com
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar>
+      </Container>
     </React.Fragment>
   );
 };
